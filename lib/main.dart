2f95c68a2b6d8075009cf075888bf24e97509d3a
Kimheoungjun <jump931030@naver.com>
2018-11-19 11:15:40 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'danolja',
+      theme: new ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
+        // counter didn't reset back to zero; the application is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: new LoginPage(title: 'Danolja'),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Center(
+        child: Container(
+          color: Color.fromRGBO(246, 239, 239, 1.0),
+          child: Column(
+              children: [
+                SizedBox(height:100.0,width:500.0),
+                Image.network('https://firebasestorage.googleapis.com/v0/b/mobile-project-4ee9b.appspot.com/o/danolja.png?alt=media&token=190dadf5-ba05-4de3-aa96-934e704d5443',width:320.0),
+                SizedBox(height:180.0),
+                MaterialButton(
+                  child: button('Google'),
+                  onPressed:(){},
+                  color:Colors.white,
+                ),
+                SizedBox(height:5.0),
+                MaterialButton(
+                  child: button2('Facebook'),
+                  onPressed:(){},
+                  color:Color.fromRGBO(58,89,152,1.0),
+                ),
+
+              ]
+          ),
+        )
+    );
+  }
+}
+
+Widget button(title, [ color = const Color.fromRGBO(68, 68, 76, .8) ]) {
+  return Container(
+    width: 220.0,
+    child: Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/mobile-project-4ee9b.appspot.com/o/google.png?alt=media&token=ef8cf6f7-9e14-4db7-baf9-2fc3b4fcf1fe',
+            width: 30.0,
+          ),
+          Padding(
+            child: Text(
+              "Sign in with $title",
+              style:  TextStyle(
+                fontFamily: 'Roboto',
+                color: color,
+                fontSize: 20.0
+              ),
+            ),
+            padding: new EdgeInsets.only(left: 15.0),
+          ),
+        ],
+      ),
+    ),
+  );
+}
+Widget button2(title, [ color = const Color.fromRGBO(68, 68, 76, .8) ]) {
+  return Container(
+    width: 220.0,
+    child: Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            'https://firebasestorage.googleapis.com/v0/b/mobile-project-4ee9b.appspot.com/o/facebook.png?alt=media&token=b1581425-3585-4214-ba54-909f068127b0',
+            width: 30.0,
+          ),
+          Padding(
+            child: Text(
+              "Sign in with $title",
+              style:  TextStyle(
+                  fontFamily: 'Roboto',
+                  color: Colors.white,
+                  fontSize: 17.0
+              ),
+            ),
+            padding: new EdgeInsets.only(left: 15.0),
+          ),
+        ],
+      ),
+    ),
+  );
+}
